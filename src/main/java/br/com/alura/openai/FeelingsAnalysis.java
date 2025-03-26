9283aa26ea34ff96d895844b2fd8ecd38e8ea8ba
JJAnony <jcolivati@gmail.com>
2025-03-26 14:13:04 -0300

    feat: created FeelingsAnalysis

---
+++ src/main/java/br/com/alura/openai/FeelingsAnalysis.java
@@ -0,0 +1,83 @@
+package br.com.alura.openai;
+
+import com.knuddels.jtokkit.api.ModelType;
+import com.theokanning.openai.completion.chat.ChatCompletionRequest;
+import com.theokanning.openai.completion.chat.ChatMessage;
+import com.theokanning.openai.completion.chat.ChatMessageRole;
+import com.theokanning.openai.service.OpenAiService;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class FeelingsAnalysis {
+
+    public static void main(String[] args) {
+
+        var systemPrompt = """
+                Você é um analisador de sentimentos de avaliações de produtos.
+                Escreva um parágrafo com até 50 palavras resumindo as avaliações e depois atribua qual o sentimento geral para o produto.
+                Identifique também 3 pontos fortes e 3 pontos fracos identificados a partir das avaliações.
+                
+                #### Formato de saída
+                Nome do produto:
+                Resumo das avaliações: [resuma em até 50 palavras]
+                Sentimento geral: [deve ser: POSITIVO, NEUTRO ou NEGATIVO]
+                Pontos fortes: [3 bullets points]
+                Pontos fracos: [3 bullets points]
+                """;
+
+        try {
+
+            var dirReviews = Path.of("src/main/resources/reviews");
+            var reviewsFiles = Files.walk(dirReviews, 1)
+                    .filter(path -> path.toString().endsWith(".txt"))
+                    .collect(Collectors.toList());
+
+            for (Path file : reviewsFiles) {
+                System.out.println("Iniciando analise do produto: " + file.getFileName());
+                var userPrompt = loadFile(file);
+
+                var request = ChatCompletionRequest
+                        .builder()
+                        .model(ModelType.GPT_4.getName())
+                        .messages(Arrays.asList(
+                                new ChatMessage(ChatMessageRole.SYSTEM.value(), systemPrompt),
+                                new ChatMessage(ChatMessageRole.USER.value(), userPrompt)))
+                        .build();
+
+                var key = System.getenv("OPENAI_API_KEY");
+                var service = new OpenAiService(key, Duration.ofSeconds(60));
+
+                var response = service
+                        .createChatCompletion(request)
+                        .getChoices().get(0).getMessage().getContent();
+
+                saveAnalysis(file.getFileName().toString(), response);
+                System.out.println("Analise Finalizada!");
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static String loadFile(Path path) {
+        try {
+            return Files.readAllLines(path).toString();
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao carregar o arquivo!", e);
+        }
+    }
+
+    private static void saveAnalysis(String file, String analise) {
+        try {
+            var path = Path.of("src/main/resources/analysis/analysis_" + file);
+            Files.writeString(path, analise, StandardOpenOption.CREATE_NEW);
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao salvar o arquivo!", e);
+        }
+    }
+}
